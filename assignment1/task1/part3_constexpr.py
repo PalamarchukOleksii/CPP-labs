572a018b74524cc7bc9f4ab60092e30878ecbcf5
Anastasiia Chichik <116720360+lavandosya@users.noreply.github.com>
2025-10-19 21:18:49 +0200

    Finished task1 part3 (#28)

--- assignment1/task1/part3_constexpr.py
+++ assignment1/task1/part3_constexpr.py
@@ -1,4 +1,7 @@
 import math
+import ast
+import inspect
+import textwrap
 
 #############################################
 #############################################
@@ -8,20 +11,98 @@ import math
 # Implement `constexpr` and `eval_const_exprs`
 
 def constexpr(func):
+    func.is_marked_constexpr = True
+    return func
+
+
+class ConstExpressionEvaluator(ast.NodeTransformer):
+    def __init__(self, global_namespace):
+        self.global_namespace = global_namespace
     
-    def modified_function(*args, **kwargs):
-        # run original func
-        return func(*args, **kwargs)
-
-    return modified_function
-
-
-def eval_const_exprs(func):
+    def visit_Call(self, node):
+        self.generic_visit(node)
+        
+        if isinstance(node.func, ast.Name):
+            function_name = node.func.id
+            
+            if function_name in self.global_namespace:
+                function = self.global_namespace[function_name]
+                
+                if hasattr(function, 'is_marked_constexpr') and function.is_marked_constexpr:
+                    if self.are_all_args_const(node):
+                        args = [self.eval_const(arg) for arg in node.args]
+                        keyword_args = {keyword.arg: self.eval_const(keyword.value) for keyword in node.keywords}
+                        
+                        result = function(*args, **keyword_args)
+                        
+                        return ast.Constant(value=result)
+        
+        return node
     
-    def modified_function(*args, **kwargs):
-        # run original func
-        return func(*args, **kwargs)
+    def are_all_args_const(self, node):
+        for argument in node.args:
+            if not self.is_const(argument):
+                return False
+        
+        for keyword_argument in node.keywords:
+            if not self.is_const(keyword_argument.value):
+                return False
+        
+        return True
+     
+    def is_const(self, node):
+        if isinstance(node, ast.Constant):
+            return True
+        elif isinstance(node, (ast.BinOp, ast.UnaryOp)):
+            return self.is_const_expression(node)
+        return False
+    
+    def is_const_expression(self, node):
+        if isinstance(node, ast.Constant):
+            return True
+        elif isinstance(node, ast.BinOp):
+            return self.is_const_expression(node.left) and self.is_const_expression(node.right)
+        elif isinstance(node, ast.UnaryOp):
+            return self.is_const_expression(node.operand)
+        return False
+    
+    def eval_const(self, node):
+        if isinstance(node, ast.Constant):
+            return node.value
+        elif isinstance(node, (ast.BinOp, ast.UnaryOp)):
+            expression = ast.Expression(body=node)
+            compiled_code = compile(expression, '<string>', 'eval')
+            return eval(compiled_code)
+        return None
 
+
+def eval_const_exprs(function):
+    source = inspect.getsource(function)
+    source = textwrap.dedent(source)
+    
+    syntax_tree = ast.parse(source)
+    
+    namespace = function.__globals__.copy()
+    
+    if function.__closure__:
+        closure_variables = inspect.getclosurevars(function)
+        namespace.update(closure_variables.nonlocals)
+    
+    evaluator = ConstExpressionEvaluator(namespace)
+    new_syntax_tree = evaluator.visit(syntax_tree)
+    
+    ast.fix_missing_locations(new_syntax_tree)
+    
+    function_definition = new_syntax_tree.body[0]
+    function_definition.decorator_list = []
+    
+    compiled_code = compile(ast.Module(body=[function_definition], type_ignores=[]), '<string>', 'exec')
+    
+    new_namespace = {}
+    exec(compiled_code, namespace, new_namespace)
+    
+    modified_function = new_namespace[function.__name__]
+    
     return modified_function
 
 #############################################
